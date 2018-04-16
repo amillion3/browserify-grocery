8110ee533df7fe3ad3298f3c022b6a71e066bbb4
Andy <amillion3@gmail.com>
2018-04-16 18:37:20 -0500

    added get item function

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -1,4 +1,5 @@
 const genericXHR = require('./departments');
+const genericXHRItems = require('./items');
 const printToDepartmentDom = require('./departmentDom');
 const data = require('./data');
 
@@ -10,12 +11,18 @@ function loadFunction () {
   printToDepartmentDom(departmentsArray);
 }
 
+function loadItems () {
+  const itemsArray = JSON.parse(this.responseText).items;
+  console.log(itemsArray);
+}
+
 function errorFunction () {
   console.error('Something broke');
 }
 
 const initializer = () => {
   genericXHR(loadFunction, errorFunction);
+  genericXHRItems(loadItems, errorFunction);
 };
 
 module.exports = {
