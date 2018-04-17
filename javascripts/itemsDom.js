ee672483caa145dfab052d023759cee117f23c5a
Andy <amillion3@gmail.com>
2018-04-16 19:46:45 -0500

    added item display functionality and styling

--- javascripts/itemsDom.js
+++ javascripts/itemsDom.js
@@ -4,7 +4,18 @@ const itemsDomString = (items) => {
   let domString = '';
   items.forEach((item) => {
     domString += `
-          <h3>${item.name}</h3>`;
+    <div class='col-sm-3 text-center item'>
+      <div class="panel panel-warning">
+        <div class="panel-heading">
+          <h3 class="panel-title">${item.name}</h3>
+        </div>
+        <div class="panel-body">
+          <div class="row">
+            <img class="item-image" src='${item.img}'>
+          </div>
+        </div>
+      </div>
+    </div>`;
   });
   return domString;
 };
