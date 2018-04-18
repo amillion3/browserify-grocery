290efb2ca049e79b2436bc35e3d0cc3601c57c07
Andy <amillion3@gmail.com>
2018-04-17 19:07:46 -0500

    added cart button event listener, not functional yet

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@ const dataGatekeeper = require('./dataGatekeeper');
 const buttonEvents = require('./buttonEvents');
 
 buttonEvents.departmentsButton();
+buttonEvents.cartButton();
 
 dataGatekeeper.initializer();
 
